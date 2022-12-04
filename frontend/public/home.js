ff63b735dca6b0ce2ff5e52a43a1f137ef8f71a9
saravanan-s2632021 <91112016+saravanan-s2632021@users.noreply.github.com>
2022-12-04 22:37:08 +0530

    Add files via upload

---
+++ frontend/public/home.js
@@ -0,0 +1,40 @@
+// function Home(){
+//   return (
+//     <>
+//     <div className="home">
+    
+//       <p class = "sam"> txtcolor="black" <br/>
+//       header="State Bank Of India"
+//       title="Welcome to the bank"
+//       text="You can move around using the navigation bar." </p>
+    
+    
+//     </div>
+//      <div>
+//     <img src="gif.gif" width="65px"  align-item="center"className="img-fluid" alt="Responsive image"/>
+    
+//     </div>
+//     </>
+//   );  
+// }
+function Home(){
+  return (
+    <>
+    <div className="home">
+    
+      {/* txtcolor="black"
+      header="State Bank Of India"
+      title="Welcome to the bank"
+      text="SMALL SAVINGS FOR YOUR BIG FUTURE." */}
+      <img src="gif.gif" className="img-fluid" alt="Responsive image"/>
+      
+      
+    
+    </div>
+
+    {/* <div className="ss">
+    <img src="gif.gif" className="img-fluid" alt="Responsive image"/>
+    </div> */}
+    </>
+  );  
+}
